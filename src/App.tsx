d5f8f2b9a7409732b8b483b5ea60a63af626e279
Cal <calpnicklin@gmail.com>
2023-03-04 21:23:57 +0000

    tutorial progress

---
+++ src/App.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
+import IconButton from './components/IconButton';
+import CircleButton from './components/CircleButton';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function App(): JSX.Element {
+  const [selectedImage, setSelectedImage] = useState('null');
+  const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const pickImageAsync = async (): Promise<void> => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert('You did not select any image.');
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={require('../assets/images/background-image.png')}
+          selectedImage={selectedImage}
+        />
+      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon='refresh' label='Reset' onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon='save-alt'
+              label='Save'
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme='primary'
+            label='Choose a photo'
+            onPress={pickImageAsync}
+          />
+          <Button
+            label='Use this photo'
+            onPress={() => {
+              setShowAppOptions(true);
+            }}
+          />
+        </View>
+      )}
+      <StatusBar style='auto' />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58
+  },
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center'
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row'
+  }
+});
---
+++ src/components/IconButton.tsx
@@ -0,0 +1,28 @@
+import { Pressable, StyleSheet, Text } from 'react-native';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
+interface IconButtonProps {
+  icon: string;
+  label: string;
+  onPress: null;
+}
+
+export default function IconButton(props: IconButtonProps): JSX.Element {
+  return (
+    <Pressable style={styles.iconButton} onPress={props.onPress}>
+      <MaterialIcons name={props.icon} size={24} color='#fff' />
+      <Text style={styles.iconButtonLabel}>{props.label}</Text>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({
+  iconButton: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  iconButtonLabel: {
+    color: '#fff',
+    marginTop: 12
+  }
+});
